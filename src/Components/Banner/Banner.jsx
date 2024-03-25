c2ad62b1fd68cbb0bf2ceca197c46d104883e419
Sa18-f <sisazid182713@gmail.com>
2024-03-25 23:29:18 +0600

    Navbar and banner section design completed

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+
+
+const Banner = () => {
+    return (
+        <div className="bg-slate-200 lg:h-[600px] rounded-xl my-12">
+            <div className="flex justify-around items-center lg:py-20 py-12">
+                <div>
+                    <h1 className="lg:text-7xl text-4xl font-bold">Books to freshen <br /> up your bookshelf</h1>
+                    <button className="btn bg-green-500 text-white lg:mt-16 mt-10 px-7 text-lg">View The List</button>
+                </div>
+                <div className="">
+                    <img src="/src/assets/images/pngwing 1.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+
+const Header = () => {
+    const links = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/listedBooks">Listed Books</NavLink></li>
+        <li><NavLink to="/pagesToRead">Pages to Read</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {links}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost lg:text-xl font-bold text-md"><img className="lg:w-10 w-8 h-12"  src="/src/assets/images/istockphoto-1197901679-612x612.jpg" alt="" /> BookPulse</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                   {links}
+                </ul>
+            </div>
+            <div className="navbar-end lg:gap-3">
+                <a className="btn bg-green-500 text-white">Sign In</a>
+                <a className="btn bg-blue-400 text-white">Sign Up</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
