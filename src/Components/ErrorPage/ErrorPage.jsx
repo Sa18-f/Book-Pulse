619801bd250393171165657f48a3dc52206a991d
Sa18-f <sisazid182713@gmail.com>
2024-03-28 04:09:29 +0600

    Error Page Created

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+    const error = useRouteError();
     return (
-        <div>
-            <h2>Oops!!</h2>
-            <Link to="/">Go Back to home</Link>
+        <div className="max-w-[1170px]">
+            <div className="lg:ml-[700px] text-center my-96">
+                <h2 className="text-xl">{error.statusText || error.message}</h2>
+                {
+                    error.status === 404 && <div>
+                        <h3 className="text-3xl font-bold my-4">404 - Page not found</h3>
+                        <Link to="/"><button className="btn">Home</button></Link>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
