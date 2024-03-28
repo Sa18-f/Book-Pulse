988f3db0f0298a13bcf61c3f40c90942a85df237
Sa18-f <sisazid182713@gmail.com>
2024-03-28 21:42:32 +0600

    Bar charts style updated

--- src/Components/PagesToRead/PagesToRead.jsx
+++ src/Components/PagesToRead/PagesToRead.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Cell } from 'recharts';
 import { getBooks } from '../../Utility';
 const getPath = (x, y, width, height) => (
     `M${x},${y + height}
      C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
      C${x + width / 2},${y + height / 3} ${x + 2 * width / 3},${y + height} ${x + width}, ${y + height}
      Z`
-  );
-  
-  const TriangleBar = (props) => {
+);
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const TriangleBar = (props) => {
     const {
-      fill, x, y, width, height,
+        fill, x, y, width, height,
     } = props;
-  
+
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
+};
 
 
 const PagesToRead = () => {
@@ -41,8 +42,14 @@ const PagesToRead = () => {
                         width={800}
                         height={500}
                         data={chartData}
-
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
                     >
+                        <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip></Tooltip>
@@ -50,7 +57,12 @@ const PagesToRead = () => {
                             dataKey="pages"
                             fill="#8884d8"
                             shape={<TriangleBar />}
-                        />
+                            label={{ position: 'top' }}
+                        >
+                            {readChart.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
                     </BarChart>
                 </div>
             </div>
